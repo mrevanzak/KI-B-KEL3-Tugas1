d61e79c6d9bfbd126b8430b4fb31a75c5a18e2dd
Khairuddin Nasty <56571284+khairuddin-n@users.noreply.github.com>
2023-11-19 21:01:54 +0700

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -11,23 +11,27 @@ import (
 	"path/filepath"
 )
 
+// Check panics if an error is not nil
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+// CheckError logs and fatal if an error is not nil
 func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// GenerateRsaKeyPair generates an RSA key pair (private and public keys)
 func GenerateRsaKeyPair() (*rsa.PrivateKey, *rsa.PublicKey) {
 	privkey, _ := rsa.GenerateKey(rand.Reader, 2048)
 	return privkey, &privkey.PublicKey
 }
 
+// ExportRsaPrivateKeyAsPemStr exports an RSA private key as a PEM-encoded string
 func ExportRsaPrivateKeyAsPemStr(privkey *rsa.PrivateKey) string {
 	privkey_bytes := x509.MarshalPKCS1PrivateKey(privkey)
 	privkey_pem := pem.EncodeToMemory(
@@ -39,6 +43,7 @@ func ExportRsaPrivateKeyAsPemStr(privkey *rsa.PrivateKey) string {
 	return string(privkey_pem)
 }
 
+// ExportRsaPublicKeyAsPemStr exports an RSA public key as a PEM-encoded string
 func ExportRsaPublicKeyAsPemStr(pubkey *rsa.PublicKey) (string, error) {
 	pubkey_bytes, err := x509.MarshalPKIXPublicKey(pubkey)
 	if err != nil {
@@ -54,19 +59,22 @@ func ExportRsaPublicKeyAsPemStr(pubkey *rsa.PublicKey) (string, error) {
 	return string(pubkey_pem), nil
 }
 
+// GenerateAndSave generates and saves RSA keys to files
 func GenerateAndSave() (priv_pem string, pub_pem string) {
 	// Create the keys
 	priv, pub := GenerateRsaKeyPair()
 
-	// Export the keys to pem string
+	// Export the keys to PEM strings
 	priv_pem = ExportRsaPrivateKeyAsPemStr(priv)
 	pub_pem, _ = ExportRsaPublicKeyAsPemStr(pub)
 
+	// Save private key to file
 	f, err := os.Create(filepath.Join("..", "server", "server_private.key"))
 	Check(err)
 	_, err = f.WriteString(priv_pem)
 	Check(err)
 
+	// Save public key to file
 	f, err = os.Create(filepath.Join("..", "server", "server_public.key"))
 	Check(err)
 	_, err = f.WriteString(pub_pem)
@@ -75,28 +83,31 @@ func GenerateAndSave() (priv_pem string, pub_pem string) {
 	return
 }
 
+// LoadAndParse loads and parses RSA keys from files
 func LoadAndParse() (priv_key *rsa.PrivateKey, pub_key *rsa.PublicKey) {
 	privIn := make([]byte, 5000)
 	pubIn := make([]byte, 5000)
 
-	//open server_private.key on server folder
+	// Open and read the private key file
 	f, err := os.Open(filepath.Join("..", "server", "server_private.key"))
 	Check(err)
 	_, err = f.Read(privIn)
 	Check(err)
 
+	// Open and read the public key file
 	f, err = os.Open(filepath.Join("..", "server", "server_public.key"))
 	Check(err)
 	_, err = f.Read(pubIn)
 	Check(err)
 
-	// Import the keys from pem string
+	// Import the keys from PEM strings
 	priv_key, _ = ParseRsaPrivateKeyFromPemStr(string(privIn))
 	pub_key, _ = ParseRsaPublicKeyFromPemStr(string(pubIn))
 
 	return
 }
 
+// ParseRsaPrivateKeyFromPemStr parses an RSA private key from a PEM-encoded string
 func ParseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
 	block, _ := pem.Decode([]byte(privPEM))
 	if block == nil {
@@ -111,6 +122,7 @@ func ParseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
 	return priv, nil
 }
 
+// ParseRsaPublicKeyFromPemStr parses an RSA public key from a PEM-encoded string
 func ParseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode([]byte(pubPEM))
 	if block == nil {
